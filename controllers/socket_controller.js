4fdc7ddd88ce2fbfcef28fb1398f6d941f8cc0cc
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-05-17 13:52:48 +0200

    Created rooms for socket and functionallity to start game

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,13 +1,54 @@
-const debug = require('debug')('battleship-server:socket_controller');
-let io = null; // socket.io server instance
+const debug = require('debug')('battleship-server:socket_controller')
+let io = null // socket.io server instance
+
+// Array of socket rooms
+const rooms = []
+let numberOfRooms = 0
+let waitingPlayers = 0
+
+const handleDisconnect = function() {
+    debug(`Client ${this.id} left`)
+}
+
+// When a user wants to enter queue
+const handleJoinQueue = function(username, callback) {
+    debug(`Player ${username} wants to join queue`)
+
+    // If queue is empty, create a new room
+    if (waitingPlayers === 0) {
+        rooms.push( {
+            room_id: numberOfRooms++,
+            players: []
+        } )
+    }
+
+    // Find current room
+    const currentRoom = rooms[rooms.length - 1]
+
+    // Join socket room
+    this.join(currentRoom)
+
+    // Push playername to player property in the current room
+    currentRoom.players.push(username)
+
+    // Increase amount of players waiting for game
+    waitingPlayers++
+
+    // If romm is full, tell sockets in room to start their game
+    if (currentRoom.players.length === 2) {
+        io.in(currentRoom).emit('game:start', currentRoom.players)
+        waitingPlayers = 0
+    }
+}
 
 module.exports = function(socket, _io) {
 	// save a reference to the socket.io server instance
 	io = _io;
 
-	debug(`Someone joined`)
+	debug(`Someone joined ${socket.id}`)
 
 	// handle user disconnect
-	// socket.on('disconnect', handleDisconnect); 
+	socket.on('disconnect', handleDisconnect)
+    socket.on('user:join-queue', handleJoinQueue)
 
 }
